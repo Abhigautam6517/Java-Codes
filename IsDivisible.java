b649b72d78c1ef6c8425da542ae9a4336ebd5545
Abhishek Gautam <83368305+Abhigautam6517@users.noreply.github.com>
2022-09-29 23:57:37 +0530

    Add files via upload

--- IsDivisible.java
+++ IsDivisible.java
@@ -1,26 +1,29 @@
 // import java.math.BigInteger;
 
 class isdivisible{
-    static int isdivisible7(String num){
+   //  static int isdivisible7(String num){
        
-       int number = Integer.parseInt(num);
-       if(number%7==0){
-        return 1;
-       }       
-       else{
-        return 0;
-       }
-    //   BigInteger number = new BigInteger(num);
-    //   int div = number.divide(7);
-    //   if(div==0){
-    //     return 1;
-    //   }
-    //   else{
-    //     return 0;
-    //   }
+   //     int number = Integer.parseInt(num);
+   //     if(number%7==0){
+   //      return 1;
+   //     }       
+   //     else{
+   //      return 0;
+   //     }
+
+   static int isdivisible7(String num){
+        
+      int rem=0;
+      for(int i=0;i<num.length();i++)
+      {
+          rem=((rem*10)+(num.charAt(i)-'0'))%7;
+      }
+      return rem==0?1:0;
+      
    }
+ 
    public static void main(String[] args) {
-    String num = "8458855255595581";
+    String num = "210000056555";
     System.out.println(isdivisible7(num));
     
    }
